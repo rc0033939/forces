75b8173423c7b11c4869a616a5c0c84f936374c0
rc0033939 <rc0033939@CRO-24-17>
2019-03-11 14:58:28 +0000

    Add project files.

---
+++ forces/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+
+        }
+        double Cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.Acos(x) * 180 / Math.PI);
+
+        }
+    }
+}
